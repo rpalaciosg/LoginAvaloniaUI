247a786bbef50bab198e82188ecd49cb75b9276e
Richard Palacios <rpalacios@mutazuay.com>
2026-02-11 17:38:06 -0500

    feat: update main window

---
+++ LoginAvaloniaUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LoginAvaloniaUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
