ffdf13362a6b47f5ad92f7011eb47787403c6c83
Richard Palacios <rpalacios@mutazuay.com>
2026-02-12 16:45:20 -0500

    feat(App): enable debug mode with devtools

--- LoginAvaloniaUI/App.axaml.cs
+++ LoginAvaloniaUI/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace LoginAvaloniaUI
 {
@@ -9,6 +10,9 @@ namespace LoginAvaloniaUI
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+
+            // esto muestra border de todos los contenderes solo para depuracion luego borrar                        
+            //RenderOptions.SetTextRenderingMode(this, TextRenderingMode.SubpixelAntialias);
         }
 
         public override void OnFrameworkInitializationCompleted()
@@ -16,6 +20,8 @@ namespace LoginAvaloniaUI
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
+                // Habilita devtools - para debug UI
+                desktop.MainWindow.AttachDevTools();
             }
 
             base.OnFrameworkInitializationCompleted();
